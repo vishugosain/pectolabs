5f6538ed0913956604c06cd34e2757b9396cf7fc
Vishal Gosain <gosain.vishal437@gmail.com>
2022-03-08 23:56:23 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      'primary-1': '#E5E3C9',
+      'primary-2': '#B4CFB0',
+      'primary-3': '#94B49F',
+      'primary-4': '#789395',
+    },
+  },
+}
